bb6fe2f77e7cf8b700577188c72b8a0063d580fd
Conor Mac Aoidh <conor.macaoidh@gonitro.com>
2024-12-09 14:25:45 +0000

    added dist

---
+++ dist/packages/nx-electron/src/utils/versions.d.ts
@@ -0,0 +1,2 @@
+export declare const nxElectronVersion = "^19.0.0";
+export declare const electronVersion = "^31.0.1";
---
+++ dist/packages/nx-electron/src/utils/versions.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.electronVersion = exports.nxElectronVersion = void 0;
+exports.nxElectronVersion = '^19.0.0';
+exports.electronVersion = '^31.0.1';
+//# sourceMappingURL=versions.js.map
